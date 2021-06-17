332397f10fa0a363e975713e91883bb1d38d14e8
Terrance Phifer <tercphif@gmail.com>
2021-06-17 18:44:31 -0500

    Initial Commit
    Making my first commit to deploy to Heroku

---
+++ server.js
@@ -0,0 +1,14 @@
+// Install Express Server
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Server only are static files from the dist directory
+app.use(express.static('./dist/my-first-app-on-heroku'));
+
+app.get('/*', (req, res) =>
+res.sendFile('index.html', {root: 'dist/my-first-app-on-heroku'}),
+);
+
+// Tell the app how to start on Heroku
+app.listen(process.env.PORT || 8080);
